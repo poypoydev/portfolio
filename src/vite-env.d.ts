d9e0bef6ca12e2af4f243b2965d42ae7a6ed78dd
Jeremiah Pantaras <jeremiahpantaras@Poypoy-MacBook-Air-M3.local>
2025-06-13 11:14:45 +0800

    Initial commit

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BASE_PATH?: string;
+  // add other env variables here if needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
